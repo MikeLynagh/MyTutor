c04930f93b8398bbd5dfca4d5254e68f42bccbe7
Mike Lynagh <mlynagh111@gmail.com>
2026-06-27 09:20:46 +0100

    feat adding boilerplate frontend files

---
+++ frontend/my-app/app/missions/[missionId]/layout.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+type MissionLayoutProps = {
+  children: ReactNode;
+};
+
+export default function MissionLayout({ children }: MissionLayoutProps) {
+  return children;
+}
